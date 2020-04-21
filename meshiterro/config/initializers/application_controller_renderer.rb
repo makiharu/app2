d8c4110276a867289045c376027600635a776f62
makiharu <kitakazetoshirokuma@gmail.com>
2020-04-21 05:03:52 +0000

    [Finish]5-3.4

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '91da55b029c095ebfc3422e1ce55576de36bd14a87711f450514017190f65c60e0d86fe961bd64d0d096bdb8cfb3bdb9760995812001ffb23572e06e8449f61d'
